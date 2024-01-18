a1af5ce51c27e9d0bdfc3e560b5a3a35678ee27e
onurtufekci34 <onurtufekci61@gmail.com>
2024-01-18 19:35:02 +0100

    some updates

--- src/pages/ProduktDashboard.jsx
+++ src/pages/ProduktDashboard.jsx
@@ -1,36 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ProduktDashboard = ({ initialProducts }) => {
   const [products, setProducts] = useState(initialProducts);
   const [newProduct, setNewProduct] = useState({
-    title: '',
+    title: "",
     price: 0,
-    description: '',
-    category: '',
-    image: '',
+    description: "",
+    category: "",
+    image: "",
   });
   const [editingProduct, setEditingProduct] = useState(null);
 
   const addProduct = (event) => {
     event.preventDefault();
-    if (newProduct.title && newProduct.price && newProduct.description && newProduct.category && newProduct.image) {
+    if (
+      newProduct.title &&
+      newProduct.price &&
+      newProduct.description &&
+      newProduct.category &&
+      newProduct.image
+    ) {
       const newProductWithId = {
         ...newProduct,
         id: products.length,
       };
       setProducts([...products, newProductWithId]);
       setNewProduct({
-        title: '',
+        title: "",
         price: 0,
-        description: '',
-        category: '',
-        image: '',
+        description: "",
+        category: "",
+        image: "",
       });
     }
   };
 
   const deleteProduct = (productId) => {
-    const updatedProducts = products.filter((product) => product.id !== productId);
+    const updatedProducts = products.filter(
+      (product) => product.id !== productId
+    );
     setProducts(updatedProducts);
   };
 
@@ -54,44 +62,99 @@ const ProduktDashboard = ({ initialProducts }) => {
     setProducts(updatedProducts);
     setEditingProduct(null);
     setNewProduct({
-      title: '',
+      title: "",
       price: 0,
-      description: '',
-      category: '',
-      image: '',
+      description: "",
+      category: "",
+      image: "",
     });
   };
 
   return (
     <div className="container mt-4">
-      <form  onSubmit={addProduct}>
+      <form onSubmit={addProduct}>
         <div className="col mb-2">
-            <label htmlFor="title" className="form-label text-success"> Title</label>
-          <input id="title" className="form-control" type="text" name="title" placeholder="Title" value={newProduct.title} onChange={handleInputChange} />
+          <label htmlFor="title" className="form-label text-success">
+            {" "}
+            Title
+          </label>
+          <input
+            id="title"
+            className="form-control"
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={newProduct.title}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="col mb-2">
-            <label htmlFor="price" className="form-label text-success">Price</label>
-          <input id='price' className="form-control" type="number" name="price" placeholder="Price" value={newProduct.price} onChange={handleInputChange} />
+          <label htmlFor="price" className="form-label text-success">
+            Price
+          </label>
+          <input
+            id="price"
+            className="form-control"
+            type="number"
+            name="price"
+            placeholder="Price"
+            value={newProduct.price}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="col mb-2">
-            <label htmlFor="description" className="form-label text-success">Description</label>
-          <input id='description' className="form-control" type="text" name="description" placeholder="Description" value={newProduct.description} onChange={handleInputChange} />
+          <label htmlFor="description" className="form-label text-success">
+            Description
+          </label>
+          <input
+            id="description"
+            className="form-control"
+            type="text"
+            name="description"
+            placeholder="Description"
+            value={newProduct.description}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="col mb-2">
-            <label htmlFor="category" className="form-label text-success">Category</label>
-          <select id='category' className="form-select" name="category" value={newProduct.category} onChange={handleInputChange}>
+          <label htmlFor="category" className="form-label text-success">
+            Category
+          </label>
+          <select
+            id="category"
+            className="form-select"
+            name="category"
+            value={newProduct.category}
+            onChange={handleInputChange}
+          >
             <option value="">Select Category</option>
             <option value="business">Business</option>
             <option value="gaming">Gaming</option>
           </select>
         </div>
         <div className="col mb-2">
-            <label htmlFor="image" className="form-label text-success">Image</label>
-          <input id='image' className="form-control" type="text" name="image" placeholder="Image URL" value={newProduct.image} onChange={handleInputChange} />
+          <label htmlFor="image" className="form-label text-success">
+            Image
+          </label>
+          <input
+            id="image"
+            className="form-control"
+            type="text"
+            name="image"
+            placeholder="Image URL"
+            value={newProduct.image}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="col d-flex justify-content-center mt-3">
-          <button className="btn btn-primary me-2" type="submit">Add Product</button>
-          {editingProduct && <button className="btn btn-primary" onClick={updateProduct}>Save Changes</button>}
+          <button className="btn btn-primary me-2" type="submit">
+            Add Product
+          </button>
+          {editingProduct && (
+            <button className="btn btn-primary" onClick={updateProduct}>
+              Save Changes
+            </button>
+          )}
         </div>
       </form>
 
@@ -100,14 +163,32 @@ const ProduktDashboard = ({ initialProducts }) => {
           <div key={product.id} className="col-lg-4 mb-3">
             <div className="card">
               <div className="card-body">
-              <div className="cardimage d-flex justify-content-center">
-            <img src={product.image} className="card-img-top w-50" alt={product.description} />
-          </div>
+                <div className="cardimage d-flex justify-content-center">
+                  <img
+                    src={product.image}
+                    className="card-img-top w-50"
+                    alt={product.description}
+                  />
+                </div>
                 <h5 className="card-title text-truncate">{product.title}</h5>
                 <p className="card-text text-truncate">{product.description}</p>
-                <p className="card-text">{product.category}</p>
-                <button className="btn btn-danger me-2" onClick={() => deleteProduct(product.id)}>Delete</button>
-                <button className="btn btn-primary" onClick={() => handleEditProduct(product)}>Edit</button>
+                <div className="d-flex justify-content-between">
+                  <p className="card-text">{product.category}</p>
+                  <p className="card-text fs-5 text-danger">{product.price}€</p>
+                </div>
+
+                <button
+                  className="btn btn-danger me-2"
+                  onClick={() => deleteProduct(product.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleEditProduct(product)}
+                >
+                  Edit
+                </button>
               </div>
             </div>
           </div>
